d55acdd6a160dc0e768212dc2de9c885056807b8
develephant <cbyerley@develephant.com>
2013-10-23 14:49:06 -0500

    Windows and OS X respawn window fix

--- on_build.py
+++ on_build.py
@@ -10,12 +10,13 @@ class OnBuildCommand(sublime_plugin.WindowCommand):
 		file_regex = args['file_regex']
 
 		if platform == 'windows' and arch == "x64":
-			cmd = ["C:\\Program Files (x86)\\Corona Labs\\Corona SDK\\Corona Simulator.exe", cmd[1]]
+			cmd = ["C:\\Program Files (x86)\\Corona Labs\\Corona SDK\\Corona Simulator.exe", "/singleton", "/no-console", cmd[1]]
 		elif platform == 'windows':
-			cmd = ["C:\\Program Files\\Corona Labs\\Corona SDK\\Corona Simulator.exe", cmd[1]]
-		elif platform == 'osx':
-			self.window.run_command("save") #for now just save to force simulator
+			cmd = ["C:\\Program Files\\Corona Labs\\Corona SDK\\Corona Simulator.exe", "-singleton", "/no-console", cmd[1]]
+		#elif platform == 'osx':
+			#self.window.run_command("save") #for now just save to force simulator
 
-		if (not cmd == None) and (not platform == 'osx'):
-			self.window.run_command('exec', { 'kill' : True }) #close sim if open
+		#if (not cmd == None) and (not platform == 'osx'):
+		if not cmd == None:
+			#self.window.run_command('exec', { 'kill' : True }) #close sim if open
 			self.window.run_command('exec', {'cmd': cmd, 'file_regex': file_regex})
